a29268d757f26739c3ac235191c8c47c6bf19657
anastasiya-bkv <51928452+anastasiya-bkv@users.noreply.github.com>
2020-05-22 01:32:26 +0500

    Add files via upload

---
+++ precache-manifest.997c74bee9bf71ea6f915b55da940c16.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37c2981a95a20aef69f96c43dcea22a1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2e7c300b777cae3dec5e",
+    "url": "/static/css/main.7ff4d920.chunk.css"
+  },
+  {
+    "revision": "6b4532c7937aa63b6a96",
+    "url": "/static/js/2.2fe4a63d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.2fe4a63d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e7c300b777cae3dec5e",
+    "url": "/static/js/main.de9f2545.chunk.js"
+  },
+  {
+    "revision": "3e33fdc84e7df65d0676",
+    "url": "/static/js/runtime-main.9543ec56.js"
+  },
+  {
+    "revision": "0c617a970b0b66a859785dbc285e145c",
+    "url": "/static/media/input.0c617a97.svg"
+  }
+]);
